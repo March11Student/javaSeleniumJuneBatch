c335239dd4e5ff028002ca221025c6c791f495d3
Zayan <sadidzayan@gmail.com>
2023-08-30 22:32:36 -0400

    update calculator class

--- src/test/java/fundamentals/Calculator.java
+++ src/test/java/fundamentals/Calculator.java
@@ -1,37 +1,26 @@
 package fundamentals;
 
 public class Calculator {
-    //comment
-
-  /*  * line one
-    * Line Two
-    * */
 
     public static void main(String[] args) {
-    //calculator();
-    //  calculator(45,60);
-
-
-        addition(30, 70);
+    validateWhileLoop();
+    validateDoWhileLoop();
     }
 
-    public static void calculator(){
-        int fExam=70;
-        int sExam=30;
-        int t=fExam+sExam;
-        System.out.println("result from 1st method: "+t);
-    }
-    public static void addition(int a,int  b ){
-        int result=a+b;
-        System.out.println("result from 2nd method: "+result);
-    }
 
-    public static int calculator(int fExam,int sExam,int tExam ){
-        int result=fExam+sExam+tExam;
-        System.out.println("result from calculation: "+result);
-        return result;
-    }
 
+    public static void validateWhileLoop(){
+        int count=11;
+        while(count==10){
+            System.out.println("output from while loop: "+count);
+        }
+    }
+    public static void validateDoWhileLoop(){
+        int count=11;
+        do{
+            System.out.println("output from do-while loop: "+count);
+        }while (count==10);
+    }
 
 
 }
